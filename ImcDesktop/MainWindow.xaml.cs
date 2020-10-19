f43d54afc8a2d2f2e1060a374e5bc7d33cdb6a08
felipe-cutz <19470219@itcampeche.edu.mx>
2020-10-18 22:41:01 -0500

    Se agregó el manejo de eventos de los tres botones.

--- ImcDesktop/MainWindow.xaml.cs
+++ ImcDesktop/MainWindow.xaml.cs
@@ -23,6 +23,98 @@ namespace ImcDesktop
         public MainWindow()
         {
             InitializeComponent();
+
+            pesoTextBox.Focus();
+        }
+
+        private void calcularButtom_Click(object sender, RoutedEventArgs e)
+        {
+            string s = pesoTextBox.Text;
+            decimal peso = Convert.ToDecimal(s);
+            s = estaturaTextBox.Text;
+            decimal estatura = Convert.ToDecimal(s);
+            decimal imc = peso / (estatura * estatura);
+            imcTextBlock.Text = string.Format("{0:.0000}", imc);
+            imcTextBlock.Foreground = SetColorEstadoNutricional(imc);
+            situacionTextBlock.Text = EstadoNutricional(imc);
+            situacionTextBlock.Foreground = SetColorEstadoNutricional(imc);
+            limpiarButton.Focus();
+        }
+
+
+
+        private void limpiarButton_Click(object sender, RoutedEventArgs e)
+        {
+            pesoTextBox.Text = "";
+            estaturaTextBox.Text = "";
+            imcTextBlock.Text = "0.0";
+            imcTextBlock.Foreground = Brushes.Black;
+            situacionTextBlock.Text = "";
+            situacionTextBlock.Foreground = Brushes.Black;
+            pesoTextBox.Focus();
+
+        }
+
+        private void salirButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private string EstadoNutricional(decimal imc)
+        {
+            if (imc < 18.5M)
+            {
+                return "Peso Bajo";
+            }
+
+            else if (imc < 25.0M)
+            {
+                return "Peso Normal";
+            }
+
+            else if (imc < 30.0M)
+            {
+                return "Sobrepeso";
+            }
+
+            else if (imc < 40.0M)
+            {
+                return "Obesidad";
+            }
+
+            else
+            {
+                return "Obsediad Extrma";
+            }
+        }
+
+        private Brush SetColorEstadoNutricional(decimal imc)
+        {
+            if (imc <18.5M)
+            {
+                return Brushes.Yellow;
+            }
+
+            else if (imc <2.0M)
+            {
+                return Brushes.Green;
+            }
+
+            else if (imc < 30.0M)
+            {
+                return Brushes.Yellow;
+            }
+
+            else if (imc < 40.0M)
+            {
+                return Brushes.Orange;
+            }
+
+            else
+            {
+                return Brushes.Red;
+            }
+           
         }
     }
 }
